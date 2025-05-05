a9974f7ba05007c227de749e2993b610d6a034ad
Kartik <kartik_a@mt.iitr.ac.in>
2025-05-06 04:36:00 +0530

    added secret

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   env:{
     AUTH_SECRET: process.env.AUTH_SECRET,
+    AZURE_OPENAI_API_KEY: process.env.AZURE_OPENAI_API_KEY,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    AZURE_OPENAI_ENDPOINT: process.env.AZURE_OPENAI_ENDPOINT,
+    RESEARCH_AGENT_URL: process.env.RESEARCH_AGENT_URL,
+    REDIS_URL: process.env.REDIS_URL,
   },
   experimental: {
     ppr: false,
