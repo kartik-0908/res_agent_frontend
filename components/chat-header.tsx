227db43d4bdaf66afb70cbc3981c491931720aaf
Kartik <kartik_a@mt.iitr.ac.in>
2025-05-12 10:04:39 +0530

    bug fix

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -106,7 +106,7 @@ function PureChatHeader({
 
 
 
-      {!isReadonly && (
+      {/* {!isReadonly && (
         <ModelSelector
           session={session}
           selectedModelId={selectedModelId}
@@ -120,7 +120,7 @@ function PureChatHeader({
           selectedVisibilityType={selectedVisibilityType}
           className="order-1 md:order-3"
         />
-      )}
+      )} */}
 
       {/* <Button
         className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
